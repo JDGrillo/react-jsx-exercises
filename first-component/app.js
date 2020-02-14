f678e5257d5609c1548afc78c266be3343e0b59a
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2020-02-13 21:03:12 -0600

    form rough draft

--- first-component/app.js
+++ first-component/app.js
@@ -3,4 +3,4 @@ let FirstComponent = () => {
     return <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Windows_logo_-_2002%E2%80%932012_%28Multicolored%29.svg/1161px-Windows_logo_-_2002%E2%80%932012_%28Multicolored%29.svg.png"></img></div>
 }
 
-ReactDOM.render(<FirstComponent />, document.getElementById('root'))
+ReactDOM.render(<FirstComponent/>, document.getElementById('root'))
--- form/app.js
+++ form/app.js
@@ -1,17 +1,41 @@
 let TextInput = () => {
-    return <span>Make this component render a styled text input tag</span>
+    // return <span>Make this component render a styled text input tag</span>
+    return <input></input>
 }
 
 let YesNoRadio = () => {
-    return <span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
+    // return <span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
+    return (
+        <div>
+            <label>
+                <input type="radio" value="yes" />
+                Yes
+    </label>
+            <label>
+                <input type="radio" value="yes" />
+                No
+    </label>
+        </div>
+    )
 }
 
 let SubmitButton = () => {
-    return <span>Make this component render a styled button of type "submit"</span>
+    // return <span>Make this component render a styled button of type "submit"</span>
+    return <button>Submit</button>
 }
 
 let Form = () => {
-    return <span>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button </span>
+    // return <span>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button </span>
+    return (
+        <div>
+            <TextInput />
+            <TextInput />
+            <TextInput />
+            <YesNoRadio />
+            <YesNoRadio />
+            <SubmitButton />
+        </div>
+    )
 }
 
 
